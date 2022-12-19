96b224abf5759a5db488ee919434902f1444d3f0
orestgluchy <orest.gluchy@webgang.io>
2022-12-19 20:15:44 +0100

    Fixed table link

--- src/pages/TablesPage.jsx
+++ src/pages/TablesPage.jsx
@@ -128,7 +128,7 @@ const TablesPage = () => {
                                         width: '100%',
                                         height: 'auto'
                                     }}
-                                    value={`https://orderwise-8213e.web.app/table/${table.id}`}
+                                    value={`https://order-wise-c3dhad3ev-orestiiik.vercel.app/table/${table.id}`}
                                 />
                             </Grid>
                         </Grid>
